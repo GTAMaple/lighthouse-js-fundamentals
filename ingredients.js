bbf2244be1644a4103b4514b44d1c0f0274d9d35
GTAMaple <sasi.pjn@gmail.com>
2021-09-03 19:18:24 +0000

    How to Iterating Over an Array With for and while

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+let j = 0;
+while(j < ingredients.length){
+  //console.log(ingredients[j]);
+  j++;
+
+}
+// Write a for loop that prints out the contents of ingredients:
+for(let i = 0; i < ingredients.length; i++){
+ // console.log(ingredients[i]);
+}
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+for( let a = ingredients.length-1; a >=0; a--)
+  console.log(ingredients[a]);
